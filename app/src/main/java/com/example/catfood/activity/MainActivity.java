3616256548f4a2b23da73d382bf64a0430dcd7ad
IssacKing123 <anhnguyen1222004@gmail.com>
2024-10-22 22:02:26 +0700

    01ve

--- app/src/main/java/com/example/catfood/activity/MainActivity.java
+++ app/src/main/java/com/example/catfood/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.catfood;
+package com.example.catfood.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.catfood.R;
 
 public class MainActivity extends AppCompatActivity {
 
